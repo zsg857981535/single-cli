bd0af92bbd27acc5c9e4ac89ddfc03eb9a0783b7
zsg857981535 <857981535@qq.com>
2018-02-25 23:44:17 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Building a Javascript Commond Line Interface(cli) with Node.js for creating h5, server, web template
---
+++ index.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+
+/*
+  todo list
+  1. 选择生成项目类型:  h5, server, web
+  2. 填写仓库名称
+  3. 填写仓库描述
+  4. 创建本地文件夹, 拉取模板
+  5. 检查gitlab权限, 创建远程仓库, 添加init commit
+  6. push 到 init commit到远程仓库
+*/
