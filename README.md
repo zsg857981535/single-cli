64daa2088bb068b7270253aa74d265041ee39d0d
zhoushengguo <zhoushengguo@iboxpay.com>
2018-02-26 18:43:39 +0800

    finished cli

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# Building a Javascript Commond Line Interface(cli) with Node.js for creating h5, server, web template
+## Building a Javascript Commond Line Interface(cli)
+## with Node.js for creating h5, server, web template
--- index.js
+++ index.js
@@ -2,10 +2,90 @@
 
 /*
   todo list
-  1. 选择生成项目类型:  h5, server, web
-  2. 填写仓库名称
-  3. 填写仓库描述
-  4. 创建本地文件夹, 拉取模板
-  5. 检查gitlab权限, 创建远程仓库, 添加init commit
-  6. push 到 init commit到远程仓库
+  github鉴权
+  选择生成项目类型:  h5, server, web
+  填写仓库名称
+  填写仓库描述
+  创建本地文件夹, 拉取模板
+  创建远程仓库, 添加init commit
+  push init commit到远程仓库
 */
+
+const chalk     = require('chalk')
+const clear     = require('clear')
+const figlet    = require('figlet')
+
+const github    = require('./lib/github')
+const repo      = require('./lib/repo')
+const files     = require('./lib/files')
+
+
+clear()
+console.log(
+  chalk.yellow(
+    figlet.textSync('single', { horizontalLayout: 'full' })
+  )
+)
+
+if (files.directoryExists('.git')) {
+  console.log(chalk.red('Already a git repository!'))
+  process.exit()
+}
+
+const getGithubToken = async () => {
+  // Fetch token from config store
+  let token = github.getStoredGithubToken()
+  if (token) {
+    return token
+  }
+  // No token found, use credentials to access github account
+  await github.setGithubCredentials()
+
+  // Check if access token for single-cli was registered
+  const accessToken = await github.hasAccessToken()
+  if (accessToken) {
+    console.log(chalk.yellow('An existing access token has been found!'))
+    // ask user to regenerate a new token
+    token = await github.regenerateNewToken(accessToken.id);
+    return token;
+  }
+  // No access token found, register one now
+  token = await github.registerNewToken()
+  return token
+}
+
+const run = async () => {
+  try {
+    // Retrieve & Set Authentication token
+    const token = await getGithubToken()
+    github.githubAuth(token)
+    console.log(
+      chalk.green('Sucessfully authenticated!')
+    )
+    // Create remote repository
+    const url = await repo.createRemoteRepo()
+
+    // TODO git clone remote template repository to local
+
+    // Setup local repository and push to remote
+    const done = await repo.setupRepo(url)
+    if (done) {
+      console.log(chalk.green('All done'))
+    }
+  } catch (err) {
+    if (err) {
+      switch (err.code) {
+        case 401:
+          console.log(chalk.red('Couldn\'t log you in. Please provide correct credentials/token.'))
+          break
+        case 422:
+          console.log(chalk.red('There already exists a remote repository with the same name'))
+          break
+        default:
+          console.log(err)
+      }
+    }
+  }
+}
+
+run()
